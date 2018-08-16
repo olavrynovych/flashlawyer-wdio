8fbe43f448186ee36cc3f7a80f41761ce7bb54d8
Oleh Lavrynovych <oleh.lavrynovych@eleks.com>
2018-08-16 16:21:02 +0300

    Finish first test with validation of existing Send email button

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,27 +1,35 @@
 let MainPage = require('../../pages/main.page');
+let LoginPage = require('../../pages/flashlawyerlogin.page');
+let GoogleLoginPage = require('../../pages/googlelogin.page');
+let DocumentPage = require('../../pages/document.page');
 
-// describe('First test',function(){
-//     it('#1. Verification.',function(){
-//         MainPage.open();
-//         MainPage.navigateToMyDocuments();
-//         expect(browser.getTitle()).toBe('All Legal Document Generators and Chatbots');
-//         //browser.getTitle().should.be.equal('All Legal Document Generators and Chatbots');
-//     })
-// });
 
-describe('First test',function(){
-    it('#1. Verification.',function(){
-        //toolbar.navigateToCreateDocuments();
+describe('First test - ',function(){
+    it('Validate "Send via Email" button exists after saving document.',function(){
+        let docName = 'Vehicle Bill of Sale';
+        //1,2 Open browser and go to Main page
         MainPage.open();
         let toolbar = MainPage.toolbar;
-        toolbar.navigateToMyDocuments();
-        expect(browser.getTitle()).toBe('All Legal Document Generators and Chatbots');
-
+        //3. Go to My Profile
         toolbar.navigateToMyProfile();
-        expect(browser.getTitle()).toBe('Login or Signup | FlashLawyer');
+        //4. Click Google button
+        LoginPage.openGoogleLogin();
+        //5. Set login
+        //6. Click next button
+        //7. Set password
+        //8. Click next button
+        GoogleLoginPage.login('automation.program1@gmail.com', 'Rjcnhjvf*~');
 
-        toolbar.navigateToMainPage();
-        expect(browser.getTitle()).toBe('Legal Document Generators Powered by AI | FlashLawyer');
-        //browser.getTitle().should.be.equal('All Legal Document Generators and Chatbots');
+        //toolbar.navigateToMyDocuments();
+        //expect(browser.getTitle()).toBe('All Legal Document Generators and Chatbots');
+        //9. Open create documents menu
+        toolbar.openCreateDocumentsMenu();
+
+        //10. Click on document link
+        toolbar.clickOnDocumentLink(docName);
+        //11. Click Save document button
+        DocumentPage.saveDocument();
+        
+        expect(DocumentPage.existSendViaEmailButton()).toBe(true);
     })
 });
